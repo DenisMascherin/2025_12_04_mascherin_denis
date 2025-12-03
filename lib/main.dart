59bbc489f0f85a116e5c6ed22379efcecab48060
Denis Mascherin <denis.mascherin.24@stud.itsaltoadriatico.it>
2025-12-03 12:40:39 +0100

    modifiche

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
